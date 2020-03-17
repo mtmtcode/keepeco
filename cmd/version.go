4ed745f8a180ca4e9d25bf264f66fec3a2d2b1d6
l3msh0 <L3msh0@gmail.com>
2020-03-18 01:53:09 +0900

    Add version subcommand

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var version = "unknown"
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version",
+	Long:  "Print version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version)
+	},
+}
